b872d537e849696466ef328167fff8200ad126ba
ElijahLaMoon <ilya.datsyuk@gmail.com>
2020-01-14 02:22:01 +0200

    add menu

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ int main()
 {
 	std::cout << "Welcome to Enigma!" << std::endl;
 	Enigma enigma;
-	enigma.start();
+	char choice;
+	bool menu = false;
+	do
+	{
+		enigma.start();
+		std::cout << "Would you like to encrypt something else? (Y)es or (N)o: ";
+		std::cin >> choice;
+		if (choice == 'Y' || choice == 'y') menu = true;
+		std::cout << std::endl;
+	} while (menu);
+	
+	std::cout << "Press Enter to exit..." << std::endl;
+	std::cin.get();
 
 	return EXIT_SUCCESS;
 }
